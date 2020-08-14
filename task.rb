0ebcedcc6e1ba47105cd00d6994cb77ece331b6d
minori <mignorita@yahoo.co.jp>
2020-08-14 09:32:04 +0900

    Q8からQ10を修正

--- task.rb
+++ task.rb
@@ -64,7 +64,7 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-  programming_languages.map! { |word| word.capitalize }
+  programming_languages.map! &:capitalize
   upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
@@ -77,14 +77,14 @@ def q9
   # 以下に回答を記載
   names.each.with_index(1) do |name, i|
   puts "会員No.#{i} #{name}さん"
-
+  end
 end
 
 def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-  if foods.include?("うに")
+  if foods.any?("うに")
     puts "好物です"
   else
     puts "はまぁまぁ好きです"
