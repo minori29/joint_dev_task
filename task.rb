09dd758a032f141c830e38626a8ef2eee76ccaee
minori <mignorita@yahoo.co.jp>
2020-08-18 09:35:29 +0900

    Q15とQ16の回答

--- task.rb
+++ task.rb
@@ -128,7 +128,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-
+  puts data1.include?(:age) ? "OK" : "NG"
+  puts data2.include?(:age) ? "OK" : "NG"
 end
 
 def q16
@@ -140,6 +141,9 @@ def q16
   ]
 
   # 以下に回答を記載
+  users.each do |user|
+    puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}です。"
+  end
 
 end
 
